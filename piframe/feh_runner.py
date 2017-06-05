cc967c88a51128c153fe86a4e91106d74e557267
James Hall <jmh045000@utdallas.edu>
2017-06-04 23:34:12 -0400

    Basic x starting and feh running

---
+++ piframe/feh_runner.py
@@ -0,0 +1,37 @@
+
+import os
+import subprocess
+import time
+
+class FehRunner(object):
+    blackhole = open('/dev/null', 'w')
+    def __init__(self):
+        self.proc = None
+        self.command_base = [ 'feh', '--full-screen' ]
+        self.command_env = {
+            'XAUTHORITY': os.path.expanduser('~/.XAuthority'),
+            'DISPLAY': ':0.0',
+            'HOME': os.path.expanduser('~')
+        }
+
+    @property
+    def running(self):
+        return self.proc is not None and self.proc.poll() is None
+
+    def next_image(self, image_path):
+        old_proc = None
+        if self.running:
+            old_proc = self.proc
+        self.proc = subprocess.Popen(
+                self.command_base + [image_path],
+                env=self.command_env,
+                stdout=self.blackhole,
+                stderr=self.blackhole)
+        time.sleep(2)
+        if old_proc:
+            old_proc.kill()
+
+    def stop(self):
+        if self.running:
+            self.proc.kill()
+            self.proc = None
---
+++ piframe/x_wrapper.py
@@ -0,0 +1,17 @@
+
+import subprocess
+import threading
+
+class XServer(object):
+    blackhole = open('/dev/null', 'w')
+    def __init__(self):
+        self.command = [ 'X', '-s', ':0.0', '-dpms', '-nocursor', '-quiet' ]
+        self.proc = None
+
+    @property
+    def running(self):
+        return self.proc is not None and self.proc.poll() is None
+
+    def start(self):
+        if not self.running:
+            self.proc = subprocess.Popen(self.command, stdout=self.blackhole, stderr=self.blackhole)
