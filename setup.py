5ade6cf82b6b7d5f9ad50ea39749c699de2fa41f
James Hall <james.m.f.hall@gmail.com>
2018-08-27 19:30:05 -0400

    remove psutil

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages("piframe"),
-    install_requires=["psutil", "pyyaml"],
+    install_requires=["pyyaml"],
     package_data={"piframe": ["config"]},
     entry_points={"console_scripts": ["piframe=piframe.main:main"]},
 )
